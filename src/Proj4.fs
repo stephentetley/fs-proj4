556339ee299783fbc585185ad914b468740bcd0e
stephentetley <stephen.tetley@gmail.com>
2018-04-11 23:31:18 +0100

    Initial code.

---
+++ src/Proj4.fs
@@ -0,0 +1,13 @@
+﻿module Proj4
+
+open System
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+
+[<System.Runtime.InteropServices.DllImport(@"C:\Program Files\QGIS 3.0\bin\proj.dll",EntryPoint="pj_run_selftests")>]
+extern int pj_run_selftests(int verbosity);
+
+
+//[<System.Runtime.InteropServices.DllImport(@"C:\Program Files\QGIS 3.0\bin\proj.dll",EntryPoint="pj_release")>]
+//extern string pj_release();
+    
---
+++ src/Test01.fsx
@@ -0,0 +1,6 @@
+﻿
+#load @"Proj4.fs"
+open Proj4
+
+let temp01 () = 
+    printfn "Hello World"
