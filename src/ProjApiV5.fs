909946518ce412b2a3cf8395e141c7798719e205
stephentetley <stephen.tetley@gmail.com>
2018-04-18 22:25:55 +0100

    Some work on the 5.0 API binding.

--- src/ProjApiV5.fs
+++ src/ProjApiV5.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Runtime.InteropServices
+open System.Text
 
 
 // The API is now defined in <proj.h>
@@ -10,8 +11,24 @@ open System.Runtime.InteropServices
 [<Literal>]
 let ProjDLL = @"E:\coding\libs\proj-5.0.1_bin\bin\proj.dll"
 
-let RAD_TO_DEG : double = 57.295779513082321
-let DEG_TO_RAD : double = 0.017453292519943296
+[<Struct; StructLayout(LayoutKind.Sequential)>]
+type PjUV = 
+    new (u, v) = { U2 = u; V2 = v }
+    val U2 : double
+    val V2 : double
+
+[<Struct; StructLayout(LayoutKind.Sequential)>]
+type PjXY = 
+    new (x, y) = { X2 = x; Y2 = y }
+    val X2 : double
+    val Y2 : double
+
+[<Struct; StructLayout(LayoutKind.Sequential)>]
+type PjXLP = 
+    new (lam, phi) = { Lam2 = lam; Phi2 = phi }
+    val Lam2 : double
+    val Phi2 : double
+
 
 [<Struct; StructLayout(LayoutKind.Sequential)>]
 type PjInfo = 
@@ -28,3 +45,18 @@ type PjInfo =
 
 [<DllImport(ProjDLL, EntryPoint="proj_info", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern PjInfo proj_info();
+
+type PjDirection = 
+    | PjFwd = 1
+    | PjIdent = 0
+    | PjInv = -1
+
+[<DllImport(ProjDLL, EntryPoint="proj_torad", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern double proj_torad (double angle_in_degrees);
+
+[<DllImport(ProjDLL, EntryPoint="proj_todeg", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern double proj_todeg (double angle_in_radians);
+
+
+[<DllImport(ProjDLL, EntryPoint="proj_rtodms", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern IntPtr proj_rtodms(StringBuilder s, double r, char pos, char neg);
--- src/TestV5.fsx
+++ src/TestV5.fsx
@@ -4,6 +4,7 @@ open Microsoft.FSharp.NativeInterop
 
 #load @"ProjApiV5.fs"
 open ProjApiV5
+open System.Text
 
 
 let Test_proj_info () : unit = 
@@ -14,3 +15,20 @@ let Test_proj_info () : unit =
     with
     | ex ->
         printfn "Error: Test_proj_info {%A}"  ex
+
+let Test_proj_torad () : unit = 
+    try 
+        let d45 : double = 45.0
+        Printf.printfn "%f:<degrees> = %f:<radians>" d45 (proj_torad d45)
+    with
+    | ex ->
+        printfn "Error: Test_proj_torad {%A}"  ex
+
+let Test_proj_rtodms () : unit = 
+    try 
+        let mutable sb = new StringBuilder(50)
+        ignore <| proj_rtodms(sb,-2.0,'N','S')
+        printfn "%s" (sb.ToString ())
+    with
+    | ex ->
+        printfn "Error: Test_proj_rtodms {%A}"  ex
