1d887679c5a862463e6957e5dc3878a43f3028dc
stephentetley <stephen.tetley@gmail.com>
2018-04-28 20:24:17 +0100

    Work fleshing out V5.0 API. I think all C functions now have FFI stubs but they need to be tested and may have incorrect types / marshalling.

--- src/ProjApiV5.fs
+++ src/ProjApiV5.fs
@@ -13,7 +13,7 @@ let ProjDLL = __SOURCE_DIRECTORY__ +  @"\..\lib\lib.5.0.1_x64\proj.dll"
 
 
 [<Struct; StructLayout(LayoutKind.Sequential)>]
-type P5Factors = 
+type PjFactors = 
     val MeridionalScale : double           /// h
     val ParallelScale : double             /// k
     val ArealScale : double                /// s
@@ -36,6 +36,12 @@ type P5Factors =
 type PjPtr = IntPtr
 type PjContextPtr = IntPtr
 type PjAreaPtr = IntPtr
+type PjCoordPtr = IntPtr
+
+type PjOperationsPtr = IntPtr
+type PjEllpsPtr = IntPtr
+type PjUnitsPtr = IntPtr
+type PjPrimeMeridiansPtr = IntPtr
 
 /// Geodetic, mostly spatiotemporal coordinate types
 
@@ -229,7 +235,6 @@ extern PjPtr proj_create(PjContextPtr ctx, string definition);
 [<DllImport(ProjDLL, EntryPoint="proj_create", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern PjPtr proj_create_argv(PjContextPtr ctx, int argc, string [] argv);
 
-
 // PJ  *proj_create_crs_to_crs(PJ_CONTEXT *ctx, const char *srid_from, const char *srid_to, PJ_AREA *area);
 [<DllImport(ProjDLL, EntryPoint="proj_create", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern PjPtr proj_create_crs_to_crs(PjContextPtr ctx, string srid_from, string srid_to, PjAreaPtr area);
@@ -237,6 +242,8 @@ extern PjPtr proj_create_crs_to_crs(PjContextPtr ctx, string srid_from, string s
 // PJ  *proj_destroy (PJ *P);
 [<DllImport(ProjDLL, EntryPoint="proj_destroy", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern PjPtr proj_destroy(PjPtr p);
+
+
 
 type PjDirection = 
     | PjFwd = 1
@@ -251,6 +258,29 @@ extern int proj_angular_input(PjPtr p, PjDirection dir);
 [<DllImport(ProjDLL, EntryPoint="proj_angular_output", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern int proj_angular_output(PjPtr p, PjDirection dir);
 
+//PJ_COORD proj_trans (PJ *P, PJ_DIRECTION direction, PJ_COORD coord);
+[<DllImport(ProjDLL, EntryPoint="proj_trans", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjCoord proj_trans (PjPtr p, PjDirection direction, PjCoord coord);
+
+//int proj_trans_array (PJ *P, PJ_DIRECTION direction, size_t n, PJ_COORD *coord);
+[<DllImport(ProjDLL, EntryPoint="proj_trans_array", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern int  proj_trans_array (PjPtr p, PjDirection direction, PjCoordPtr coord);
+
+//size_t proj_trans_generic (
+//    PJ *P,
+//    PJ_DIRECTION direction,
+//    double *x, size_t sx, size_t nx,
+//    double *y, size_t sy, size_t ny,
+//    double *z, size_t sz, size_t nz,
+//    double *t, size_t st, size_t nt
+//);
+[<DllImport(ProjDLL, EntryPoint="proj_trans_generic", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern int  proj_trans_generic (PjPtr p, PjDirection direction, 
+    [<In;Out>] double[] x, int sx, int nx,
+    [<In;Out>] double[] y, int sy, int ny, 
+    [<In;Out>] double[] z, int sz, int nz,
+    [<In;Out>] double[] t, int st, int nt);
+
 
 /// Initializers
 // PJ_COORD proj_coord (double x, double y, double z, double t);
@@ -260,7 +290,8 @@ extern PjCoord proj_coord(double x, double y, double z, double t);
 
 // Measure internal consistency - in forward or inverse direction
 // double proj_roundtrip (PJ *P, PJ_DIRECTION direction, int n, PJ_COORD *coord);
-
+[<DllImport(ProjDLL, EntryPoint="proj_roundtrip", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern double proj_roundtrip (PjPtr p, PjDirection direction, int n, PjCoordPtr b);
 
 // Geodesic distance between two points with angular 2D coordinates
 // double proj_lp_dist (const PJ *P, PJ_COORD a, PJ_COORD b);
@@ -284,6 +315,10 @@ extern double proj_xy_dist (PjCoord a, PjCoord b);
 [<DllImport(ProjDLL, EntryPoint="proj_xyz_dist", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern double proj_xyz_dist (PjCoord a, PjCoord b);
 
+// Geodesic distance (in meter) + fwd and rev azimuth between two points on the ellipsoid
+// PJ_COORD proj_geod (const PJ *P, PJ_COORD a, PJ_COORD b);
+[<DllImport(ProjDLL, EntryPoint="proj_geod", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjCoord proj_geod (PjPtr p ,PjCoord a, PjCoord b);
 
 /// Set or read error level
 
@@ -308,6 +343,14 @@ extern int proj_errno_reset(PjPtr ctx);
 extern int proj_errno_restore(PjPtr ctx, int err);
 
 
+
+// Scaling and angular distortion factors
+// PJ_FACTORS proj_factors(PJ *P, PJ_COORD lp);
+[<DllImport(ProjDLL, EntryPoint="proj_factors", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjFactors proj_factors(PjPtr ctx, PjCoord lp);
+
+
+
 /// Info functions - get information about various PROJ.4 entities 
 
 // PJ_INFO proj_info(void);
@@ -327,6 +370,24 @@ extern PjGridInfo proj_grid_info(string gridname);
 [<DllImport(ProjDLL, EntryPoint="proj_init_info", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
 extern PjInitInfo proj_init_info(string initname);
 
+// List functions: 
+// Get lists of operations, ellipsoids, units and prime meridians. 
+// const PJ_OPERATIONS       *proj_list_operations(void);
+[<DllImport(ProjDLL, EntryPoint="proj_list_operations", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjOperationsPtr proj_list_operations();
+
+// const PJ_ELLPS            *proj_list_ellps(void);
+[<DllImport(ProjDLL, EntryPoint="proj_list_ellps", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjEllpsPtr proj_list_ellps();
+
+// const PJ_UNITS            *proj_list_units(void);
+[<DllImport(ProjDLL, EntryPoint="proj_list_units", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjUnitsPtr proj_list_units();
+
+// const PJ_PRIME_MERIDIANS  *proj_list_prime_meridians(void);
+[<DllImport(ProjDLL, EntryPoint="proj_list_prime_meridians", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjPrimeMeridiansPtr proj_list_prime_meridians();
+
 
 /// Helpers 
 
@@ -343,6 +404,9 @@ extern double proj_todeg (double angle_in_radians);
 /// Geographical to geocentric latitude - another of the "simple, but useful"
 
 // PJ_COORD proj_geocentric_latitude (const PJ *P, PJ_DIRECTION direction, PJ_COORD coord);
+[<DllImport(ProjDLL, EntryPoint="proj_geocentric_latitude", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
+extern PjCoord proj_geocentric_latitude (PjPtr p, PjDirection direction, PjCoord coord);
+
 
 // double proj_dmstor(const char *is, char **rs);
 [<DllImport(ProjDLL, EntryPoint="proj_dmstor", CharSet=CharSet.Ansi, CallingConvention=CallingConvention.Cdecl)>]
