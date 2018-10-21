c9a5aeeae8e10ccb8fb4aef603088b281dd70f58
stephentetley <stephen.tetley@gmail.com>
2018-10-21 18:25:07 +0100

    Adding missed files from last commit...

---
+++ test/Demo01.fsx
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+open System
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+
+open System.Text
+
+
+#load "..\src\FsProj4\RawFFI.fs"
+open FsProj4.RawFFI
+
+let demo01 () : bool = 
+    try 
+        //
+        let ctx : PjContextPtr = proj_context_create ();
+        if ctx <> IntPtr.Zero then
+            let pj : PjPtr = proj_create_crs_to_crs(ctx, "epsg:4326", "epsg:27700", IntPtr.Zero) 
+            if pj <> IntPtr.Zero then 
+                printfn "proj_create OK"
+                proj_destroy(pj) |> ignore
+                proj_context_destroy(ctx) |> ignore
+                true
+
+            else 
+                printfn "proj_create_crs_to_crs (%A)" pj
+                proj_context_destroy(ctx) |> ignore
+                failwith "Error: proj_create_crs_to_crs "
+        else
+            failwith "Error: proj_create (proj_context_create)" 
+           
+
+    with
+    | ex ->
+        failwithf "Error: Test_proj_create {%A}"  ex
+        
---
+++ test/Demo02.fsx
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+open System
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+
+open System.Text
+
+
+#load "..\src\FsProj4\RawFFI.fs"
+open FsProj4.RawFFI
+
+
+/// from the gie.c test suite
+
+let demo01 () : bool = 
+    try 
+        //
+        let ctx : PjContextPtr = proj_context_create ();
+        if ctx = IntPtr.Zero then 
+            failwith "proj_context_create"
+        else 
+            printfn "context (%A)" ctx
+
+        let pj : PjPtr = proj_create_crs_to_crs(ctx, "epsg:25832", "epsg:25833", IntPtr.Zero) 
+        if pj = IntPtr.Zero then 
+            failwith "proj_create_crs_to_crs"
+        else    
+            printfn "proj (%A)" pj
+            proj_destroy(pj) |> ignore
+            proj_context_destroy(ctx) |> ignore
+            true         
+
+    with
+    | ex ->
+        failwithf "Error: Test_proj_create {%A}"  ex
+        
